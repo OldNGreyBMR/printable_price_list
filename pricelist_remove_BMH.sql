8dd571b6b13ede46938238404bf1a62be07d0f9f
OldNGrey <73047349+OldNGreyBMR@users.noreply.github.com>
2024-01-10 13:05:46 +1000

    Delete pricelist_remove_BMH.sql
    cleanup

--- pricelist_remove_BMH.sql
+++
@@ -1,5 +0,0 @@
-# for use in PHPMyAdmin where tabel prefix zen_ is used NOT zen cart admin module REMOVE THIS LINE before use
-DELETE FROM zen_configuration WHERE configuration_key LIKE 'PL_%';
-DELETE FROM zen_configuration_group WHERE configuration_group_title='Printable Price-list';
-DELETE FROM zen_configuration_group WHERE configuration_group_title LIKE 'Price-list Profile-%';
-DELETE FROM zen_admin_pages WHERE page_key LIKE 'config%Pricelist%';
