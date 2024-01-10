b805133b42633d1e7c4adf6ae8ed0fb3dca66d5d
OldNGrey <73047349+OldNGreyBMR@users.noreply.github.com>
2024-01-10 12:57:43 +1000

    rename folders

--- pricelist_remove_BMH.sql
+++ pricelist_remove_BMH.sql
@@ -1,3 +1,4 @@
+# for use in PHPMyAdmin where tabel prefix zen_ is used NOT zen cart admin module REMOVE THIS LINE before use
 DELETE FROM zen_configuration WHERE configuration_key LIKE 'PL_%';
 DELETE FROM zen_configuration_group WHERE configuration_group_title='Printable Price-list';
 DELETE FROM zen_configuration_group WHERE configuration_group_title LIKE 'Price-list Profile-%';
