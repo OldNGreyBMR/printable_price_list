52e9fc80e311982f8e37636720f883b276474b9a
OldNGrey <73047349+OldNGreyBMR@users.noreply.github.com>
2026-09-07 11:30:33 +1000

    Update manifest.php

--- manifest.php
+++ manifest.php
@@ -5,7 +5,7 @@ return [
     'pluginDescription' => 'This drop-in plugin provides a printable price-list for your Zen Cart store. Up to 3 separate variations can be customized; see the plugin\'s readme.html file for additional information.',
     'pluginAuthor' => 'Vinos de Frutas Tropicales (lat9), updated by OldNGrey',
     'pluginId' => 173, // ID from Zen Cart forum
-    'zcVersions' => ['v158a', 'v200, v210, v220, v221, v222, v230'],
+    'zcVersions' => ['v210, v220, v221, v222, v230'],
     'changelog' => '', // online URL (eg github release tag page, or changelog file there) or local filename only, ie: changelog.txt (in same dir as this manifest file)
     'github_repo' => 'https://github.com/lat9/printable_price_list', // url
     'pluginGroups' => [],
